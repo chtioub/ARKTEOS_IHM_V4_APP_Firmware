abe7b718987262f806912dfcf0ae2cb8bfe16e89
Ark DEV <samuel.ernaut@arkteos.com>
2024-09-24 14:37:29 +0200

    Test SER

--- TouchGFX/gui/include/gui/synoptique_screen/SynoptiqueView.hpp
+++ TouchGFX/gui/include/gui/synoptique_screen/SynoptiqueView.hpp
@@ -11,7 +11,20 @@ public:
     virtual ~SynoptiqueView() {}
     virtual void setupScreen();
     virtual void tearDownScreen();
-protected:
-};
+    virtual void changeStatutEther(S_CYC_ETHER_III *sCycEther);
+    virtual void changeStatutPAC(S_STATUT_PAC *sStatut_PAC);
+    virtual void changeDate(S_DATE *sDate);
+    virtual void changeErreur(uint16_t u16Erreur);
+    virtual void changeStatutCyclFrigo(S_CYCL_REG_FRI *sCyclRegFrigo);
+    virtual void bouton_retour();
 
+protected:
+    bool bConnexionDistance;
+    uint16_t u16ErreurAffichee;
+    S_DATE sDate_old;
+    S_CONFIG_IHM sConfig_IHM_old;
+    S_STATUT_PAC sStatut_PAC_old;
+    touchgfx::Unicode::UnicodeChar textAreaBuffer_Titre[40], textAreaBuffer_Heure[6],  textAreaBuffer_Date[9], textAreaBuffer_Temp_Dep_Prim[7];
+    S_CYCL_REG_FRI sCyclRegFrigo_old;
+};
 #endif // SYNOPTIQUEVIEW_HPP
--- TouchGFX/gui/src/synoptique_screen/SynoptiquePresenter.cpp
+++ TouchGFX/gui/src/synoptique_screen/SynoptiquePresenter.cpp
@@ -7,6 +7,7 @@ SynoptiquePresenter::SynoptiquePresenter(SynoptiqueView& v)
 
 }
 
+
 void SynoptiquePresenter::activate()
 {
 
@@ -16,3 +17,30 @@ void SynoptiquePresenter::deactivate()
 {
 
 }
+
+void SynoptiquePresenter::changeStatutEther(S_CYC_ETHER_III *sCycEther)
+{
+	view.changeStatutEther(sCycEther);
+}
+
+void SynoptiquePresenter::changeStatutPAC(S_STATUT_PAC *sStatut_PAC)
+{
+	view.changeStatutPAC(sStatut_PAC);
+}
+
+void SynoptiquePresenter::changeDate(S_DATE *sDate)
+{
+	view.changeDate(sDate);
+}
+
+void SynoptiquePresenter::changeErreur(uint16_t u16Erreur)
+{
+	view.changeErreur(u16Erreur);
+}
+
+void SynoptiquePresenter::changeStatutCyclFrigo(S_CYCL_REG_FRI *sCyclRegFrigo)
+{
+	view.changeStatutCyclFrigo(sCyclRegFrigo);
+}
+
+
